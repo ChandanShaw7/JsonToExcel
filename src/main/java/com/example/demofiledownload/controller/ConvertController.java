462110cc5a448b561174be7c389bcf291ecaeaaa
Chandan <chandan.shaw@kuliza.com>
2019-12-05 18:55:44 +0530

    first commit

--- src/main/java/com/example/demofiledownload/controller/ConvertController.java
+++ src/main/java/com/example/demofiledownload/controller/ConvertController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import sun.awt.image.ImageWatched;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
@@ -47,82 +48,125 @@ public class ConvertController {
         Set<String> allkeysValue = new HashSet<>() ;
 
         FileReader jsonFile = new FileReader(fetchfile);
+
         Object obj = parser.parse(jsonFile);
         JSONArray jsonDetails = (JSONArray)obj;
         String json = jsonDetails.toString();
         List<TreeMap> map = objm.readValue(json, List.class);
+//        System.out.println(map);
+        Set<String> inside = new HashSet<>();
 
         for(Map em: map){
-            allkeysValue.addAll(em.keySet());
+//            Set<String> insideKey = new HashSet<>();
+            inside.addAll(em.keySet());
+            for(Object key: em.keySet()){
+                String keyVal = (String) key;
+                if (em.get(key) instanceof LinkedHashMap){
+                    inside.remove(key);
+                    inside.addAll(parseMap((LinkedHashMap) em.get(key),(String)key, keyVal));
+                }
+            }
+//            inside.addAll(insideKey);
+            allkeysValue.addAll(inside);
         }
+        System.out.println(allkeysValue);
 
 
         ArrayList<String> list = new ArrayList<>(allkeysValue);
 
         System.out.println(list);
 
-        XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet = workbook.createSheet("student Details");
-
-        int rownum = 0;
-        Row row = sheet.createRow(rownum++);
-        int cellnum = 0;
-//        Cell cell = row.createCell(cellnum++);
-        for (int i = 0; i < list.size(); i++){
-            Cell cell = row.createCell(cellnum++);
-            cell.setCellValue(list.get(i));
-        }
-        cellnum = 0;
-        int len = list.size();
-        for(Map em: map){
-            row = sheet.createRow(rownum++);
-            cellnum = 0;
-            for(int i = 0; i < list.size(); i++){
-                Cell cell = row.createCell(cellnum++);
-                if (!em.containsKey(list.get(i))){
-                    cell.setCellValue("");
-                } else{
-                    cell.setCellValue((String)em.get(list.get(i)));
-                }
-            }
-
-        }
-        try {
-            // this Writes the workbook gfgcontribute
-            FileOutputStream out = new FileOutputStream(new File(String.valueOf(fileNameAndPath.getParent())+"/"+"gfgcontribute.xlsx"));
-            workbook.write(out);
-            out.close();
-            System.out.println("gfgcontribute.xlsx written successfully on disk.");
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Path excelFileNameAndPath = Paths.get(uploadDirectory, "gfgcontribute.xlsx");
-        System.out.println("Excel file");
-        System.out.println(excelFileNameAndPath.getParent());
-        System.out.println(excelFileNameAndPath.getFileName());
-
-        File excelFile = new File(String.valueOf(excelFileNameAndPath.getParent())+"/"+String.valueOf(excelFileNameAndPath.getFileName()));
-        System.out.println(excelFile.getName());
-        System.out.println(excelFile.getParent());
-
-        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" +excelFile.getName());
-
-        // Content-Length
-        response.setContentLength((int) excelFile.length());
-
-        BufferedInputStream inStream = new BufferedInputStream(new FileInputStream(excelFile));
-        BufferedOutputStream outStream = new BufferedOutputStream(response.getOutputStream());
-
-        byte[] buffer = new byte[1024];
-        int bytesRead = 0;
-        while ((bytesRead = inStream.read(buffer)) != -1) {
-            outStream.write(buffer, 0, bytesRead);
-        }
-        outStream.flush();
-        inStream.close();
+//        XSSFWorkbook workbook = new XSSFWorkbook();
+//        XSSFSheet sheet = workbook.createSheet("student Details");
+//
+//        int rownum = 0;
+//        Row row = sheet.createRow(rownum++);
+//        int cellnum = 0;
+////        Cell cell = row.createCell(cellnum++);
+//        for (int i = 0; i < list.size(); i++){
+//            Cell cell = row.createCell(cellnum++);
+//            cell.setCellValue(list.get(i));
+//        }
+//        cellnum = 0;
+//        int len = list.size();
+//        for(Map em: map){
+//            row = sheet.createRow(rownum++);
+//            cellnum = 0;
+//            for(int i = 0; i < list.size(); i++){
+//                Cell cell = row.createCell(cellnum++);
+//                if (!em.containsKey(list.get(i))){
+//                    cell.setCellValue("");
+//                } else{
+//                    cell.setCellValue(String.valueOf(em.get(list.get(i))));
+//                }
+//            }
+//
+//        }
+//        try {
+//            // this Writes the workbook gfgcontribute
+//            FileOutputStream out = new FileOutputStream(new File(String.valueOf(fileNameAndPath.getParent())+"/"+"gfgcontribute.xlsx"));
+//            workbook.write(out);
+//            out.close();
+//            System.out.println("gfgcontribute.xlsx written successfully on disk.");
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        Path excelFileNameAndPath = Paths.get(uploadDirectory, "gfgcontribute.xlsx");
+//        System.out.println("Excel file");
+//        System.out.println(excelFileNameAndPath.getParent());
+//        System.out.println(excelFileNameAndPath.getFileName());
+//
+//        File excelFile = new File(String.valueOf(excelFileNameAndPath.getParent())+"/"+String.valueOf(excelFileNameAndPath.getFileName()));
+//        System.out.println(excelFile.getName());
+//        System.out.println(excelFile.getParent());
+//
+//        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" +excelFile.getName());
+//
+//        // Content-Length
+//        response.setContentLength((int) excelFile.length());
+//
+//        BufferedInputStream inStream = new BufferedInputStream(new FileInputStream(excelFile));
+//        BufferedOutputStream outStream = new BufferedOutputStream(response.getOutputStream());
+//
+//        byte[] buffer = new byte[1024];
+//        int bytesRead = 0;
+//        while ((bytesRead = inStream.read(buffer)) != -1) {
+//            outStream.write(buffer, 0, bytesRead);
+//        }
+//        outStream.flush();
+//        inStream.close();
 
     }
 
+    private Set<String> parseMap(LinkedHashMap em, String key, String Val){
+        Set<String> allKeys = new HashSet<>();
+        Boolean flag = false;
+        parse(em,allKeys,flag, Val);
+        return allKeys;
+    }
+
+    private void parse(LinkedHashMap em, Set<String> allKeys, Boolean flag, String keyVal){
+        if (flag)
+            return;
+        flag = true;
+        for(Object keys: em.keySet()) {
+            if (em.get(keys) instanceof LinkedHashMap) {
+                allKeys.remove((Object)keyVal);
+                keyVal = keyVal + "/"+(String) keys;
+                System.out.println(keyVal);
+                flag = false;
+                parse((LinkedHashMap) em.get(keys), allKeys,flag, keyVal);
+                keyVal = keyVal.replaceAll("/"+(String)keys,"");
+            } else {
+                allKeys.remove((Object)keyVal);
+                allKeys.add(keyVal + "/"+(String) keys);
+            }
+        }
+    }
+
 }
+//[address/geo/lng, website, address/geo/gp/lat, company/employeeId, address, address/geo/gp/loc, address/geo/suite, address/zipcode,
+//        company/name, address/geo/houseNo, address/geo/lat, address/geo/gp/yoc, address/geo/city, phone, address/geo/pos, name,
+//        company/catchPhrase, company, id, company/bs, address/geo/street, email, username]
