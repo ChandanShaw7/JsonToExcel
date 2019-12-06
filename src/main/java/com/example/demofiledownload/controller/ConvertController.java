00c758cefecdcd4ba7a174d8d0861a8816f463c5
Chandan <chandan.shaw@kuliza.com>
2019-12-06 18:34:33 +0530

    "final changes"

--- src/main/java/com/example/demofiledownload/controller/ConvertController.java
+++ src/main/java/com/example/demofiledownload/controller/ConvertController.java
@@ -57,7 +57,6 @@ public class ConvertController {
         Set<String> inside = new HashSet<>();
 
         for(Map em: map){
-//            Set<String> insideKey = new HashSet<>();
             inside.addAll(em.keySet());
             for(Object key: em.keySet()){
                 String keyVal = (String) key;
@@ -66,7 +65,6 @@ public class ConvertController {
                     inside.addAll(parseMap((LinkedHashMap) em.get(key),(String)key, keyVal));
                 }
             }
-//            inside.addAll(insideKey);
             allkeysValue.addAll(inside);
         }
         System.out.println(allkeysValue);
@@ -76,9 +74,9 @@ public class ConvertController {
 
         System.out.println(list);
 
-//        XSSFWorkbook workbook = new XSSFWorkbook();
-//        XSSFSheet sheet = workbook.createSheet("student Details");
-//
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("student Details");
+
 //        int rownum = 0;
 //        Row row = sheet.createRow(rownum++);
 //        int cellnum = 0;
@@ -102,42 +100,94 @@ public class ConvertController {
 //            }
 //
 //        }
-//        try {
-//            // this Writes the workbook gfgcontribute
-//            FileOutputStream out = new FileOutputStream(new File(String.valueOf(fileNameAndPath.getParent())+"/"+"gfgcontribute.xlsx"));
-//            workbook.write(out);
-//            out.close();
-//            System.out.println("gfgcontribute.xlsx written successfully on disk.");
-//        }
-//        catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        Path excelFileNameAndPath = Paths.get(uploadDirectory, "gfgcontribute.xlsx");
-//        System.out.println("Excel file");
-//        System.out.println(excelFileNameAndPath.getParent());
-//        System.out.println(excelFileNameAndPath.getFileName());
-//
-//        File excelFile = new File(String.valueOf(excelFileNameAndPath.getParent())+"/"+String.valueOf(excelFileNameAndPath.getFileName()));
-//        System.out.println(excelFile.getName());
-//        System.out.println(excelFile.getParent());
-//
-//        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" +excelFile.getName());
-//
-//        // Content-Length
-//        response.setContentLength((int) excelFile.length());
-//
-//        BufferedInputStream inStream = new BufferedInputStream(new FileInputStream(excelFile));
-//        BufferedOutputStream outStream = new BufferedOutputStream(response.getOutputStream());
-//
-//        byte[] buffer = new byte[1024];
-//        int bytesRead = 0;
-//        while ((bytesRead = inStream.read(buffer)) != -1) {
-//            outStream.write(buffer, 0, bytesRead);
-//        }
-//        outStream.flush();
-//        inStream.close();
 
+
+
+
+        int rownum = 0;
+        Row row = sheet.createRow(rownum++);
+        int cellnum = 0;
+        for (int i = 0; i < list.size(); i++){
+            Cell cell = row.createCell(cellnum++);
+            cell.setCellValue(list.get(i));
+        }
+        cellnum = 0;
+        int len = list.size();
+        for(Map em: map){
+            row = sheet.createRow(rownum++);
+            cellnum = 0;
+            for(int i = 0; i < list.size(); i++){
+                String[] mainKey = list.get(i).split("/");
+                Cell cell = row.createCell(cellnum++);
+                if(mainKey.length == 1){
+                    if (!em.containsKey(list.get(i))){
+                        cell.setCellValue("");
+                    } else{
+                        cell.setCellValue(String.valueOf(em.get(list.get(i))));
+                    }
+                } else {
+                    cell.setCellValue(jsonKeyVal(mainKey, (LinkedHashMap) em));
+                }
+            }
+
+        }
+
+
+
+        try {
+            // this Writes the workbook gfgcontribute
+            FileOutputStream out = new FileOutputStream(new File(String.valueOf(fileNameAndPath.getParent())+"/"+"gfgcontribute.xlsx"));
+            workbook.write(out);
+            out.close();
+            System.out.println("gfgcontribute.xlsx written successfully on disk.");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Path excelFileNameAndPath = Paths.get(uploadDirectory, "gfgcontribute.xlsx");
+        System.out.println("Excel file");
+        System.out.println(excelFileNameAndPath.getParent());
+        System.out.println(excelFileNameAndPath.getFileName());
+
+        File excelFile = new File(String.valueOf(excelFileNameAndPath.getParent())+"/"+String.valueOf(excelFileNameAndPath.getFileName()));
+        System.out.println(excelFile.getName());
+        System.out.println(excelFile.getParent());
+
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" +excelFile.getName());
+
+        // Content-Length
+        response.setContentLength((int) excelFile.length());
+
+        BufferedInputStream inStream = new BufferedInputStream(new FileInputStream(excelFile));
+        BufferedOutputStream outStream = new BufferedOutputStream(response.getOutputStream());
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = 0;
+        while ((bytesRead = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+        outStream.flush();
+        inStream.close();
+
+    }
+
+    private String jsonKeyVal(String[] allKeys, LinkedHashMap em){
+        String val = null;
+        LinkedHashMap ema = em;
+        for(String key: allKeys){
+            if (ema.get(key) instanceof LinkedHashMap){
+                ema = (LinkedHashMap) ema.get(key);
+            }
+            else{
+                if(!ema.containsKey((Object)key)){
+                    val = "";
+                }else{
+                    val = (String) ema.get(key);
+                }
+            }
+        }
+        return val;
     }
 
     private Set<String> parseMap(LinkedHashMap em, String key, String Val){
